fa8a3389ecc6a706ecfdb5027b56d0a46fcfd4f2
FaMuLan <fa_mulan@163.com>
2017-10-21 20:17:30 +0800

    文件系統，支持文件夾遍歷和搜索(屏蔽隱藏文件)

---
+++ file_system.cpp
@@ -0,0 +1,63 @@
+#include "file_system.h"
+#include <dirent.h>
+#include <fstream>
+#include <regex>
+
+void lm::ReadFile(std::string path, std::string &output)
+{
+	std::ifstream file;
+	file.open(path);
+	while (!file.eof())
+	{
+		output += file.get();
+	}
+	file.close();
+}
+
+void lm::WriteFile(std::string path, std::string &output)
+{
+	std::ofstream file;
+	file.open(path);
+	file << output;
+	file.close();
+}
+
+bool lm::ListDir(std::string path, std::vector<lm::File*> &output)
+{
+	DIR *dir = opendir(path.c_str());
+	if (dir == NULL)
+	{
+		exit(0);
+	}
+	dirent *file = NULL;
+	while ((file = readdir(dir)) != NULL)
+	{
+		if (file->d_name[0] != '.')
+		{
+			File *new_file = new File;
+			new_file->name = path + "/" + std::string(file->d_name);
+			new_file->type = file->d_type == 8 ? FILETYPE_FILE : (file->d_type == 4 ? FILETYPE_DIR : FILETYPE_LINK);
+			output.push_back(new_file);
+		}
+	}
+	return true;
+}
+
+void lm::FindFile(std::string path, std::string pattern_str, std::vector<File*> &output)
+{
+	std::vector<File*> current_list;
+	ListDir(path, current_list);
+	std::regex pattern(pattern_str); 
+	for (int i = 0; i < current_list.size(); i++)
+	{
+		if (std::regex_match(current_list[i]->name, pattern))
+		{
+			output.push_back(current_list[i]);
+		}
+		else if (current_list[i]->type == FILETYPE_DIR)
+		{
+			FindFile(current_list[i]->name, pattern_str, output);
+			//使用遞歸讀取子目錄下的文件
+		}
+	}
+}
---
+++ file_system.h
@@ -0,0 +1,29 @@
+#ifndef LUNATIC_MELODY_FILE_SYSTEM_H
+#define LUNATIC_MELODY_FILE_SYSTEM_H
+
+#include <string>
+#include <vector>
+
+namespace lm
+{
+	enum FileType
+	{
+		FILETYPE_FILE = 0,
+		FILETYPE_DIR,
+		FILETYPE_LINK
+	};
+
+	struct File
+	{
+		std::string name;
+		FileType type;
+	};
+
+	void ReadFile(std::string path, std::string &output);
+	void WriteFile(std::string path, std::string &output);
+	
+	bool ListDir(std::string path, std::vector<File*> &output);
+	void FindFile(std::string path, std::string pattern_str, std::vector<File*> &output);
+};
+
+#endif
