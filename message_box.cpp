22de5203b87c6ae15782259993e0d035b614957e
FaMuLan <fa_mulan@163.com>
2017-10-24 00:49:09 +0800

    信息欄，可以調試也可以做提示功能(包括聊天，系統提示)

---
+++ message_box.cpp
@@ -0,0 +1,60 @@
+#include "message_box.h"
+#include "texture_manager.h"
+#include "sprite.h"
+#include "timer.h"
+
+lm::MessageBox *lm::MessageBox::m_instance = 0;
+
+void lm::MessageBox::init()
+{
+	base = new Sprite;
+	base->load("assets/message_box_base.png");
+	TextureManager::instance()->loadfont("assets/Ubuntu-R.ttf", 48);
+	text_base = "none text";
+	is_base_text_showing = true;
+}
+
+void lm::MessageBox::clear()
+{
+	
+}
+
+void lm::MessageBox::update()
+{
+	if (!is_base_text_showing)
+	{
+		if (Timer::instance()->GetTime("message_box_top_showing") > top_show_duration)
+		{
+			Timer::instance()->ResetTimer("message_box_top_showing");
+			top_show_duration = 0;
+			is_base_text_showing = true;
+		}
+	}
+}
+
+void lm::MessageBox::render()
+{
+	base->render();
+	if (is_base_text_showing)
+	{
+		TextureManager::instance()->render(text_base, 360, 40, "assets/Ubuntu-R.ttf", 48, 0x00, 0x00, 0x00);
+	}
+	else
+	{
+		TextureManager::instance()->render(text_top, 360, 40, "assets/Ubuntu-R.ttf", 48, 0x00, 0x00, 0x00);
+	}
+}
+
+void lm::MessageBox::SetText(std::string text, int time)
+{
+	if (time != 0)
+	{
+		is_base_text_showing = false;
+	Timer::instance()->RunTimer("message_box_top_showing");
+		text_top = text;
+	}
+	else
+	{
+		text_base = text;
+	}
+}
---
+++ message_box.h
@@ -0,0 +1,41 @@
+#ifndef LUNATIC_MELODY_MESSAGE_BOX_H
+#define LINATIC_MELODY_MESSAGE_BOX_H
+
+#include <string>
+#include <vector>
+
+namespace lm
+{
+	class Sprite;
+
+	class MessageBox
+	{
+		public:
+			static MessageBox *instance()
+			{
+				if (m_instance == 0)
+				{
+					m_instance = new MessageBox;
+					return m_instance;
+				}
+				return m_instance;
+			}
+			void init();
+			void clear();
+			void update();
+			void render();
+			void SetText(std::string text, int time = 0);
+		private:
+			MessageBox() {}
+			~MessageBox() {}
+			static MessageBox *m_instance;
+			Sprite *base;
+			//Sprite *box;
+			std::string text_top;
+			std::string text_base;
+			bool is_base_text_showing;
+			int top_show_duration;
+	};
+};
+
+#endif
