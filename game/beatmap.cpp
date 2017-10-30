7afa1f43a299c3c2035a2ebdd7c726ee1405de63
FaMuLan <fa_mulan@163.com>
2017-10-31 02:51:35 +0800

    譜面相關，目前支持OSU! mania 4k

---
+++ game/beatmap.cpp
@@ -0,0 +1,135 @@
+#include "beatmap.h"
+#include <string>
+#include <regex>
+#include "column.h"
+#include "../message_box.h"
+#include "../file_system.h"
+#include "../system.h"
+#include "../timer.h"
+
+lm::Beatmap *lm::Beatmap::m_instance = 0;
+
+void lm::Beatmap::load(std::string path)
+{
+	note_duration = 1000;
+	offset = -200;
+
+	std::string text;
+	ReadFile(path, text);
+	std::regex note_pattern("(\\d+),\\d+,(\\d+),(\\d+),\\d+,(\\d+):\\d+:\\d+:\\d+:(\\d+:)?");
+	bool is_mapped = false;
+
+	for (std::sregex_iterator i = std::sregex_iterator(text.begin(), text.end(), note_pattern); i != std::sregex_iterator(); i++)
+	{
+		std::smatch line = *i;
+		Note *new_note = new Note;
+		new_note->time = atoi(std::regex_replace(line.str(), note_pattern, "$2").c_str()) + 2000;
+		int type = atoi(std::regex_replace(line.str(), note_pattern, "$3").c_str());
+		new_note->time_end = (type % 16 == 0) ? atoi(std::regex_replace(line.str(), note_pattern, "$4").c_str()) + 2000 : new_note->time;
+		int column_index = atoi(std::regex_replace(line.str(), note_pattern, "$1").c_str());
+		if (!is_mapped)
+		{
+			switch (column_index)
+			{
+				case 64:
+				case 192:
+				case 320:
+				case 448:
+					for (int i = 0; i < 4; i++)
+					{
+						Column *new_column = new Column;
+						int w = System::instance()->GetWindowWidth() / 4;
+						new_column->init(w * i, w);
+						m_column.push_back(new_column);
+					}
+					column_mapper[64] = 0;
+					column_mapper[192] = 1;
+					column_mapper[320] = 2;
+					column_mapper[448] = 3;
+				break;
+			}
+			is_mapped = true;
+		}
+		m_column[column_mapper[column_index]]->AddNote(new_note);
+	}
+	Timer::instance()->RunTimer("game");
+}
+
+void lm::Beatmap::clear()
+{
+	
+}
+
+void lm::Beatmap::update()
+{
+	for (int i = 0; i < m_column.size(); i++)
+	{
+		m_column[i]->update();
+		m_column[i]->render();
+	}
+}
+
+lm::Judgement lm::Beatmap::judge(int note_time, bool is_pressed, bool is_ln_pressing)
+{
+	int time_diff = note_time - Timer::instance()->GetTime("game");
+	if (is_pressed)
+	{
+		if (time_diff > 1000 && !is_ln_pressing)
+		{
+			return JUDGEMENT_NONE;
+		}
+		else if (time_diff > 1000 && is_ln_pressing)
+		{
+			score += JUDGEMENT_ER;
+			combo = 0;
+			MessageBox::instance()->SetText("Error");
+			return JUDGEMENT_ER;
+		}
+		else if (time_diff > 500 || time_diff < -500)
+		{
+			score += JUDGEMENT_ER;
+			combo = 0;
+			MessageBox::instance()->SetText("Error");
+			return JUDGEMENT_ER;
+		}
+		else if (time_diff > 200 || time_diff < -200)
+		{
+			score += JUDGEMENT_GD;
+			combo++;
+			MessageBox::instance()->SetText("Good");
+			return JUDGEMENT_GD;
+		}
+		else if (time_diff > 100 || time_diff < -100)
+		{
+			score += JUDGEMENT_GR;
+			combo++;
+			MessageBox::instance()->SetText("Great");
+			return JUDGEMENT_GR;
+		}
+		else
+		{
+			score += JUDGEMENT_PG;
+			combo++;
+			MessageBox::instance()->SetText("Pure");
+			return JUDGEMENT_PG;
+		}
+	}
+	else if (time_diff < -1000)
+	{
+		score += JUDGEMENT_ER;
+		combo = 0;
+		MessageBox::instance()->SetText("Error");
+		return JUDGEMENT_ER;
+	}
+	return JUDGEMENT_NONE;
+}	//Judgement lm::Beatmap::judge()
+
+int lm::Beatmap::GetDuration()
+{
+	return note_duration;
+}
+
+int lm::Beatmap::GetOffset()
+{
+	return offset;
+}
---
+++ game/beatmap.h
@@ -0,0 +1,64 @@
+#ifndef LUNATIC_MELODY_GAME_BEATMAP_H
+#define LUNATIC_MELODY_GAME_BEATMAP_H
+
+#include <map>
+#include <vector>
+#include <string>
+
+namespace lm
+{
+	class Column;
+
+	enum Judgement
+	{
+		JUDGEMENT_NONE = 0,
+		JUDGEMENT_PG = 2,	//P GREAT
+		JUDGEMENT_GR = 1,	//GREAT
+		JUDGEMENT_GD = 0,	//GOOD
+		JUDGEMENT_ER = -5	//ERROR
+	};
+	//這樣枚舉就可以直接跟分數放一起運算了
+	//特別提醒:為了拉開分數差距，GOOD不加分，ERROR會扣分
+
+	struct Note
+	{
+		int time;
+		int time_end;
+	};
+
+	class Beatmap
+	{
+		public:
+			static Beatmap *instance()
+			{
+				if (m_instance == 0)
+				{
+					m_instance = new Beatmap;
+					return m_instance;
+				}
+				return m_instance;
+			}
+			void load(std::string path);
+			void clear();
+			void update();
+			Judgement judge(int note_time, bool is_pressed = true, bool is_ln_pressing = false);
+			//第三個參數中考慮到ln尾判的判定與一般note不一樣
+			int GetCombo();
+			int GetScore();
+			int GetDuration();
+			int GetOffset();
+		private:
+			Beatmap() {}
+			~Beatmap() {}
+			static Beatmap *m_instance;
+			std::vector<Column*> m_column;
+			std::map<Judgement, int> judge_count;
+			std::map<int, int> column_mapper;
+			int offset;
+			int note_duration;
+			int score;
+			int combo;
+	};
+};
+
+#endif	//LUNATIC_MELODY_GAME_BEATMAP_H
