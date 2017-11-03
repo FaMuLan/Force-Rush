88d7ca804b918b66253170d2817104d492b13634
FaMuLan <fa_mulan@163.com>
2017-11-03 22:41:26 +0800

    准备支持动态图像(未测试)

--- sprite.cpp
+++ sprite.cpp
@@ -1,10 +1,11 @@
 #include "sprite.h"
 #include "texture_manager.h"
+#include "timer.h"
 
-void lm::Sprite::load(std::string path, int x, int y, int w, int h)
+void lm::Sprite::init(std::string path, int x, int y, int w, int h)
 {
 	TextureManager::instance()->load(path, m_w, m_h);
-	m_path = path;
+	frame.push_back(path);
 	m_x = x;
 	m_y = y;
 	if (w != 0 || h != 0)
@@ -13,21 +14,57 @@ void lm::Sprite::load(std::string path, int x, int y, int w, int h)
 		m_h = h;
 	}
 	scale = 1;
+	base_index = 0;
+	start_time = 0;
+	duration = 0;
+	current_index = 0;
 }
 
 void lm::Sprite::update()
 {
-	//do nothing
+	if (duration != 0)
+	{
+		current_index = (frame_end - frame_start) != 0 ? ((Timer::instance()->GetSystemTime() - start_time) / duration % (frame_end + 1 - frame_start) + frame_start) : frame_start;
+		if (Timer::instance()->GetSystemTime() - start_time >= duration)
+		{
+			duration = 0;
+			start_time = 0;
+		}
+	}
+	else
+	{
+		current_index = base_index;
+	}
 }
 
 void lm::Sprite::render()
 {
-	TextureManager::instance()->render(m_path, m_x, m_y, m_w * scale, m_h * scale);
+	TextureManager::instance()->render(frame[current_index], m_x, m_y, m_w * scale, m_h * scale);
 }
 
 void lm::Sprite::clear()
 {
-	TextureManager::instance()->clear(m_path);
+//	TextureManager::instance()->clear(m_path);
+}
+
+void lm::Sprite::AddFrame(std::string path)
+{
+	int temp_w, temp_h;
+	TextureManager::instance()->load(path, temp_w, temp_h);
+	frame.push_back(path);
+}
+
+void lm::Sprite::SetAnimate(int load_start, int load_end, int load_duration)
+{
+	frame_start = load_start;
+	frame_end = load_end;
+	duration = load_duration;
+	start_time = Timer::instance()->GetSystemTime();
+}
+
+void lm::Sprite::SetBaseFrame(int index)
+{
+	base_index = index;
 }
 
 void lm::Sprite::SetPos(int x, int y)
@@ -49,4 +86,14 @@ int lm::Sprite::GetX()
 int lm::Sprite::GetY()
 {
 	return m_y;
+}
+
+int lm::Sprite::GetW()
+{
+	return m_w;
+}
+
+int lm::Sprite::GetH()
+{
+	return m_h;
 }
--- sprite.h
+++ sprite.h
@@ -2,23 +2,35 @@
 #define LUNATIC_MELODY_SPRITE_H
 
 #include <string>
+#include <vector>
 
 namespace lm
 {
 	class Sprite
 	{
 		public:
-			virtual void load(std::string path, int x = 0, int y = 0, int w = 0, int h = 0);
+			virtual void init(std::string path, int x = 0, int y = 0, int w = 0, int h = 0);
 			virtual void update();
 			virtual void render();
 			virtual void clear();
 
+			void AddFrame(std::string path);
+			void SetAnimate(int load_start, int load_end, int load_duration);
+			void SetBaseFrame(int index);
 			void SetPos(int x, int y);
 			void SetScale(float load_scale);
 			int GetX();
 			int GetY();
+			int GetW();
+			int GetH();
 		protected:
-			std::string m_path;
+			int base_index;
+			int current_index;
+			std::vector<std::string> frame;
+			unsigned long start_time;
+			int duration;
+			int frame_start;
+			int frame_end;
 			int m_x;
 			int m_y;
 			int m_w;
