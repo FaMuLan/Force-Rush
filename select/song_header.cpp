b4ff39ed0b6cea6420f873d06f845d05dc6cd4f4
FaMuLan <fa_mulan@163.com>
2017-11-07 02:07:45 +0800

    添加顯示譜面難度，譜面版本和歌曲時長

--- select/song_header.cpp
+++ select/song_header.cpp
@@ -22,21 +22,32 @@ void lm::SongHeader::clear()
 
 void lm::SongHeader::render()
 {
+	char *difficulty_ch = new char[3];
+	char *duration_ch = new char[4];
+	sprintf(difficulty_ch, "Lv.%d", m_information->difficulty);
+	sprintf(duration_ch, "%ds", m_information->duration);
+
 	if (System::instance()->GetWindowRotation() == WINDOWROTATION_PORTRAIT)
 	{
 		header_base_portrait->render();
-		TextureManager::instance()->render(m_information->title, 32, 40, "assets/Ubuntu-R.ttf", 32, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
+		TextureManager::instance()->render(m_information->title, 32, 40, "assets/Ubuntu-R.ttf", 32, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 416);
 		TextureManager::instance()->render(m_information->artist, 32, 112, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
 		TextureManager::instance()->render(m_information->noter, 32, 176, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
 		TextureManager::instance()->render(m_information->bpm, 32, 240, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
+		TextureManager::instance()->render(m_information->version, 264, 112, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
+		TextureManager::instance()->render(difficulty_ch, 264, 176, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
+		TextureManager::instance()->render(duration_ch, 264, 240, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
 	}
 	else
 	{
 		header_base_landscape->render();
-		TextureManager::instance()->render(m_information->title, 32, 40, "assets/Ubuntu-R.ttf", 32, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
-		TextureManager::instance()->render(m_information->artist, 32, 112, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
-		TextureManager::instance()->render(m_information->noter, 32, 176, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
-		TextureManager::instance()->render(m_information->bpm, 32, 240, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 184);
+		TextureManager::instance()->render(m_information->title, 32, 40, "assets/Ubuntu-R.ttf", 32, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 464);
+		TextureManager::instance()->render(m_information->artist, 32, 112, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 232);
+		TextureManager::instance()->render(m_information->noter, 32, 176, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 232);
+		TextureManager::instance()->render(m_information->bpm, 32, 240, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 232);
+		TextureManager::instance()->render(m_information->version, 312, 112, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 232);
+		TextureManager::instance()->render(difficulty_ch, 312, 176, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 232);
+		TextureManager::instance()->render(duration_ch, 264, 240, "assets/Ubuntu-R.ttf", 24, 0x00, 0x00, 0x00, TEXTFORMAT_LEFT, 232);
 	}
 }	//void lm::SongHeader::render()
 
