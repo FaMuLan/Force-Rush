a9e2952618bfb1f0a1980640b82088e661214827
FaMuLan <fa_mulan@163.com>
2017-11-10 01:14:13 +0800

    整理使其變得更加方便

--- text_area.cpp
+++ text_area.cpp
@@ -2,24 +2,28 @@
 #include <SDL2/SDL.h>
 #include "texture_manager.h"
 
-void lm::TextArea::load(std::string text, std::string font_path, int font_size, int r, int g, int b)
+void lm::TextArea::init(std::string text, int x, int y, std::string font_path, int font_size, char r, char g, char b, TextFormat format, int limited_w)
 {
 	m_text = text;
+	m_x = x;
+	m_y = y;
 	m_font_path = font_path;
 	m_font_size = font_size;
 	m_r = r;
 	m_g = g;
 	m_b = b;
+	m_format = format;
+	m_limited_w = limited_w;
 }
 
 void lm::TextArea::render()
 {
-	TextureManager::instance()->render(m_text, m_x, m_y, m_font_path, m_font_size, m_r, m_g, m_b);
+	TextureManager::instance()->render(m_text, m_x, m_y, m_font_path, m_font_size, m_r, m_g, m_b, m_format, m_limited_w);
 }
 
 void lm::TextArea::render(int x, int y)
 {
-	TextureManager::instance()->render(m_text, x, y, m_font_path, m_font_size, m_r, m_g, m_b);
+	TextureManager::instance()->render(m_text, x, y, m_font_path, m_font_size, m_r, m_g, m_b, m_format, m_limited_w);
 }
 
 void lm::TextArea::clear()
--- text_area.h
+++ text_area.h
@@ -5,10 +5,17 @@
 
 namespace lm
 {
+	enum TextFormat
+	{
+		TEXTFORMAT_MIDDLE = 0,
+		TEXTFORMAT_LEFT = 1,
+		TEXTFORMAT_RIGHT
+	};
+
 	class TextArea
 	{
 		public:
-			virtual void load(std::string text, std::string font_path, int font_size, int r, int g, int b);
+			virtual void init(std::string text, int x, int y, std::string font_path, int font_size, char r, char g, char b, TextFormat format = TEXTFORMAT_MIDDLE, int m_limited_w = 0);
 			virtual void render();
 			virtual void render(int x, int y);
 			virtual void clear();
@@ -24,6 +31,8 @@ namespace lm
 			int m_r;
 			int m_g;
 			int m_b;
+			TextFormat m_format;
+			int m_limited_w;
 	};	//class TextArea
 };	//namespace lm
 
