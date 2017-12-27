00780e260122004876265912467bbbb7c8441efa
FaMuLan <fa_mulan@163.com>
2017-12-28 00:10:44 +0800

    借用一下作为fps计数器使用

--- message_box.cpp
+++ message_box.cpp
@@ -21,6 +21,7 @@ void fr::MessageBox::clear()
 
 void fr::MessageBox::update()
 {
+/*
 	if (!is_base_text_showing)
 	{
 		if (Timer::instance()->GetTime("message_box_top_showing") > top_show_duration)
@@ -30,6 +31,14 @@ void fr::MessageBox::update()
 			is_base_text_showing = true;
 		}
 	}
+*/
+	static int last_time = 0;
+	int current_time = Timer::instance()->GetSystemTime();
+	int fps = double(1000) / double(current_time - last_time);
+	char *fps_ch = new char[8];
+	sprintf(fps_ch, "%d", fps);
+	text_base = fps_ch;
+	last_time = current_time;
 }
 
 void fr::MessageBox::render()
@@ -47,6 +56,7 @@ void fr::MessageBox::render()
 
 void fr::MessageBox::SetText(std::string text, int time)
 {
+	/*
 	if (time != 0)
 	{
 		is_base_text_showing = false;
@@ -57,4 +67,5 @@ void fr::MessageBox::SetText(std::string text, int time)
 	{
 		text_base = text;
 	}
+	*/
 }
