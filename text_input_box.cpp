00dcb2dbdb0cda0fd89efb5f77f3ba653a301c74
FaMuLan <fa_mulan@163.com>
2018-01-27 23:16:24 +0800

    文本輸入框，目前是打開關閉鍵盤這一方面出了點問題

---
+++ text_input_box.cpp
@@ -0,0 +1,58 @@
+#include "text_input_box.h"
+#include <SDL2/SDL.h>
+#include "text_area.h"
+#include "control_handler.h"
+
+void fr::TextInputBox::clear()
+{
+	Button::clear();
+	current_text->clear();
+}
+
+void fr::TextInputBox::update()
+{
+	Button::update();
+	std::string current_text_str = current_text->GetText();
+
+	if (is_released)
+	{
+		ControlHandler::instance()->SwitchTextInput();
+	}
+
+	if (ControlHandler::instance()->IsTextInput())
+	{
+		current_text_str += ControlHandler::instance()->HearInputText();
+		if (ControlHandler::instance()->IsBackspaceDown() && current_text_str.length() > 0)
+		{
+			current_text_str.pop_back();
+		}
+		current_text->SetText(current_text_str);
+	}
+}
+
+void fr::TextInputBox::render()
+{
+	Button::render();
+	current_text->render(m_x + current_text->GetX(), m_y + current_text->GetY());
+}
+
+void fr::TextInputBox::InitText(int x, int y, std::string font_path, int font_size, char r, char g, char b, TextFormat format, int limited_w)
+{
+	current_text = new TextArea;
+	current_text->init("", x, y, font_path, font_size, r, g, b, format, limited_w);
+}
+
+std::string fr::TextInputBox::GetText()
+{
+	return current_text->GetText();
+}
+
+void fr::TextInputBox::SetText(std::string input_text)
+{
+	current_text->SetText(input_text);
+}
+
+void fr::TextInputBox::SetFont(std::string font_path, int font_size)
+{
+	current_text->SetFont(font_path, font_size);
+}
