57cdafce0a7b05c290c601e90452ab58fa68b637
FaMuLan <fa_mulan@163.com>
2018-01-31 10:48:00 +0800

    改名，更注重于个人信息的存取

---
+++ user/user_profile.h
@@ -0,0 +1,46 @@
+#ifndef FORCE_RUSH_USER_PROFILE_H
+#define FORCE_RUSH_USER_PROFILE_H
+
+#include <string>
+
+namespace fr
+{
+	class Sprite;
+
+	class UserProfile
+	{
+		public:
+			static UserProfile *instance()
+			{
+				if (m_instance == 0)
+				{
+					m_instance = new UserProfile;
+					return m_instance;
+				}
+				return m_instance;
+			}
+			void init();
+			void clear();
+			void update();
+			void RenderCharacter();
+			void RenderController();
+			std::string GetUserName();
+			int GetPerformancePoint();
+			void SetUserName(std::string input);
+			int CalculatePerformancePoint(int difficulty, double acc);
+			bool read();
+			bool write();
+		private:
+			UserProfile() {}
+			~UserProfile() {}
+			static UserProfile *m_instance;
+			std::string m_user_name;
+			int m_performance_point;
+			std::string m_char_path;
+			std::string m_controller_path;
+			Sprite *m_char;
+			Sprite *m_controller;
+	};
+};
+
+#endif
