36842027805c95b57dc70ff5afa1e59f9a62e503
FaMuLan <fa_mulan@163.com>
2018-06-26 00:32:58 +0800

    请原谅我为什么一个月都没有commit，我把SDL_mixer阉割掉然后用底层音频播放了

--- sound_manager.cpp
+++ sound_manager.cpp
@@ -1,101 +1,267 @@
 #include "sound_manager.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 fr::SoundManager *fr::SoundManager::m_instance = 0;
+std::map<std::string, fr::Sound*> fr::SoundManager::m_sound;
+std::string fr::SoundManager::current_sound_data;
+unsigned short fr::SoundManager::SoundVolume;
+
+enum mad_flow fr::mp3::input(void *data, struct mad_stream *stream)
+{
+	Sound *m_sound = (Sound*)data;
+	if (!m_sound->file_length)
+	{
+		return MAD_FLOW_STOP;
+	}
+	mad_stream_buffer(stream, m_sound->file_start, m_sound->file_length);
+	m_sound->file_length = 0;
+	return MAD_FLOW_CONTINUE;
+}
+
+inline signed int fr::mp3::scale(mad_fixed_t sample)
+{
+	sample += (1L << (MAD_F_FRACBITS - 16));
+	if (sample >= MAD_F_ONE)
+	{
+		sample = MAD_F_ONE - 1;
+	}
+	else if (sample < -MAD_F_ONE)
+	{
+		sample = -MAD_F_ONE;
+	}
+	return sample >> (MAD_F_FRACBITS + 1 - 16);
+}
+
+enum mad_flow fr::mp3::output(void *data, struct mad_header const *header, struct mad_pcm *pcm)
+{
+	unsigned int channels_count, samples_count, frequency;
+	mad_fixed_t const *left_ch, *right_ch;
+
+	Sound *m_sound = (Sound*)data;
+	SDL_AudioCVT cvt;
+	
+
+	channels_count  = pcm->channels;
+	samples_count   = pcm->length;
+	frequency = pcm->samplerate;
+	left_ch    = pcm->samples[0];
+	right_ch   = pcm->samples[1];
+	m_sound->current_buffer_duration = 2048 * 4;
+
+	static unsigned char *new_buffer = new unsigned char[2048 * 4];
+
+	static int i = 0;
+	while (samples_count--)
+	{
+		signed int sample;
+		sample = scale(*left_ch++);
+		new_buffer[i++] = (sample >> 0) & 0xFF;
+		new_buffer[i++] = (sample >> 8) & 0xFF;
+		if (channels_count == 2)
+		{
+			sample = scale(*right_ch++);
+			new_buffer[i++] = (sample >> 0) & 0xFF;
+			new_buffer[i++] = (sample >> 8) & 0xFF;
+		}
+		else
+		{
+			new_buffer[i++] = (sample >> 0) & 0xFF;
+			new_buffer[i++] = (sample >> 8) & 0xFF;
+		}
+		if (i >= 2048 * 4)
+		{
+			i = 0;
+			m_sound->buffer.push_back(new_buffer);
+			new_buffer = new unsigned char[2048 * 4];
+		}
+	}
+	SDL_BuildAudioCVT(&cvt, AUDIO_S16, channels_count, frequency, AUDIO_S16, 2, 44100);
+	cvt.buf = new_buffer;
+	cvt.len = samples_count * 4;
+	SDL_ConvertAudio(&cvt);
+	return MAD_FLOW_CONTINUE;
+}
+
+enum mad_flow fr::mp3::error(void *data, struct mad_stream *stream, struct mad_frame *frame)
+{
+	Sound* m_sound = (Sound*)data;
+	fprintf(stderr, "decoding error 0x%04x (%s) at byte offset %u\n", stream->error, mad_stream_errorstr(stream), stream->this_frame - m_sound->file_start);
+	return MAD_FLOW_CONTINUE;
+}
+
+int fr::mp3::decode(Sound *load_sound)
+{
+	struct mad_decoder m_decoder;
+	int result;
+
+	mad_decoder_init(&m_decoder, load_sound, mp3::input, 0, 0, mp3::output, mp3::error, 0);
+	mad_decoder_options(&m_decoder, 0);
+	result = mad_decoder_run(&m_decoder, MAD_DECODER_MODE_SYNC);
+	mad_decoder_finish(&m_decoder);
+
+	load_sound->sound_duration = load_sound->buffer.size() * 4096.f / 44100.f * 1000.f;
+	load_sound->sound_process = 0;
+	load_sound->current_buffer_process = 0;
+	load_sound->current_buffer_index = 0;
+
+	return result;
+}
+
+bool fr::mp3::IsMP3File(unsigned char *magic)
+{
+	if (strncmp((char*)magic, "ID3", 3) == 0)
+	{
+		return false;
+	}
+
+	if (((magic[0] & 0xFF) != 0xFF) || ((magic[1] & 0xF0) != 0xF0) || ((magic[2] & 0xF0) == 0x00) || ((magic[2] & 0xF0) == 0xF0) || ((magic[2] & 0x0C) == 0x0C) || ((magic[1] & 0x06) == 0x00))
+	{
+		return true;
+	}
+	return false;
+}
 
 void fr::SoundManager::init()
 {
+/*
 	if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG) == 0)
 	{
 		exit(0);
 	}
 	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+*/
+	SDL_AudioSpec want, have;
+	want.freq = 44100;
+	want.format = AUDIO_S16;
+	want.channels = 2;
+	want.samples = 2048;
+	want.callback = &SoundManager::AudioCallback;
+	SDL_OpenAudio(&want, &have);
+
 }
 
 void fr::SoundManager::clear()
 {
-	m_sfx.clear();
-	m_music.clear();
-	Mix_CloseAudio();
+	
 }
 
-void fr::SoundManager::clear(std::string path, SoundType type)
+void fr::SoundManager::clear(std::string path)
 {
-	if (type == SOUNDTYPE_SFX)
-	{
-		Mix_FreeChunk(m_sfx[path]);
-	}
-	if (type == SOUNDTYPE_MUSIC)
-	{
-		Mix_FreeMusic(m_music[path]);
-	}
+	
 }
 
-void fr::SoundManager::load(std::string path, SoundType type)
+bool fr::SoundManager::load(std::string path)
 {
-	if (type == SOUNDTYPE_SFX)
+	Sound *new_sound = new Sound;
+	new_sound->buffer.reserve(100000);
+	int fd = open(path.c_str(), O_RDWR);
+	void *fdm;
+	struct stat st;
+	if (fstat(fd, &st) == -1 || st.st_size == 0)
 	{
-		Mix_Chunk *new_sound = Mix_LoadWAV(path.c_str());
-		m_sfx[path] = new_sound;
+		return false;
 	}
-	if (type == SOUNDTYPE_MUSIC)
+	fdm = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	if (fdm == MAP_FAILED)
 	{
-		Mix_Music *new_music = Mix_LoadMUS(path.c_str());
-		m_music[path] = new_music;
+		return false;
 	}
+	unsigned char *file_ch = (unsigned char*)fdm;
+	new_sound->file_start = file_ch;
+	new_sound->file_length = st.st_size;
+	unsigned int magic = 0;
+	magic += file_ch[0] << 24;
+	magic += file_ch[1] << 16;
+	magic += file_ch[2] << 8;
+	magic += file_ch[3] << 0;
+	switch (magic)
+	{
+		case 0x57415645:	//WAVE
+		break;
+		case 0x4F676753:	//OGGS
+		break;
+		default:
+			if (mp3::IsMP3File((Uint8*)&magic))
+			{
+				mp3::decode(new_sound);
+			}
+			else
+			{
+				return false;
+			}
+		break;
+	}
+	//分析SDL_mixer得知，MP3格式的要另外检测
+	if (munmap(fdm, st.st_size) == -1)
+	{
+		return false;
+	}
+	m_sound[path] = new_sound;
+	return true;
 }
 
-void fr::SoundManager::play(std::string path, SoundType type)
+void fr::SoundManager::play(std::string path)
 {
-	if (type == SOUNDTYPE_SFX)
-	{
-		Mix_PlayChannel(-1, m_sfx[path], 0);
-	}
-	if (type == SOUNDTYPE_MUSIC)
-	{
-		Mix_PlayMusic(m_music[path], 0);
-		Mix_SetMusicPosition(0);
-	}
+	m_sound[path]->is_playing = true;
+	m_sound[path]->sound_process = 0;
+	SDL_PauseAudio(0);
 }
 
 void fr::SoundManager::play(std::string path, int time)
 {
-	Mix_PlayMusic(m_music[path], 0);
-	Mix_SetMusicPosition(double(time) / double(1000));
+	SDL_PauseAudio(0);
 }
 
 void fr::SoundManager::stop()
 {
-	Mix_HaltMusic();
+	SDL_PauseAudio(1);
 }
 
 void fr::SoundManager::SwitchPause()
 {
-	if (!Mix_PausedMusic())
-	{
-		Mix_PauseMusic();
-	}
-	else
-	{
-		Mix_ResumeMusic();
-	}
+	
 }
 
 void fr::SoundManager::SetVolume(Uint16 load_volume, SoundType type)
 {
-	if (type == SOUNDTYPE_MUSIC)
+	
+}
+
+void fr::SoundManager::LoadMP3(std::string path)
+{
+	
+}
+
+void fr::SoundManager::LoadOGG(std::string path)
+{
+	
+}
+
+void fr::SoundManager::AudioCallback(void *userdata, unsigned char *stream, int len)
+{
+	SDL_memset(stream, 0, len);
+	for (std::map<std::string, Sound*>::iterator iter = m_sound.begin(); iter != m_sound.end(); iter++)
 	{
-		Mix_VolumeMusic(load_volume);
-	}
-	if (type == SOUNDTYPE_SFX)
-	{
-		for (std::map<std::string, Mix_Chunk*>::iterator i = m_sfx.begin(); i != m_sfx.end(); i++)
+		Sound *load_data = iter->second;
+		if (load_data->sound_process < load_data->sound_duration && load_data->is_playing)
 		{
-			Mix_VolumeChunk(i->second, load_volume);
+			len = (load_data->current_buffer_process + len > load_data->current_buffer_duration) ? load_data->current_buffer_duration - load_data->current_buffer_process : len;
+			SDL_MixAudio(stream, load_data->buffer[load_data->current_buffer_index], len, SDL_MIX_MAXVOLUME);
+			load_data->current_buffer_process += len;
+			if (load_data->current_buffer_process >= load_data->current_buffer_duration)
+			{
+				load_data->current_buffer_index++;
+				load_data->current_buffer_process = 0;
+			}
 		}
 	}
 }
 
 bool fr::SoundManager::IsPlayingMusic()
 {
-	return Mix_PlayingMusic();
+	
 }
--- sound_manager.h
+++ sound_manager.h
@@ -2,7 +2,7 @@
 #define FORCE_RUSH_SOUND_MANAGER_H
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
+#include <mad.h>
 #include <string>
 #include <map>
 #include <vector>
@@ -13,6 +13,41 @@ namespace fr
 	{
 		SOUNDTYPE_MUSIC = 0,
 		SOUNDTYPE_SFX
+	};
+
+	enum AudioFileType
+	{
+		AUDIOFILETYPE_MP3 = 0,
+		AUDIOFILETYPE_OGG,
+		AUDIOFILETYPE_WAV
+	};
+
+	struct Sound
+	{
+		unsigned char *file_start;
+		unsigned int file_length;
+		std::vector<unsigned char*> buffer;
+		unsigned long current_buffer_index;
+		unsigned int current_buffer_duration;
+		unsigned int current_buffer_process;
+		unsigned int sound_duration;
+		unsigned int sound_process;
+		bool is_playing;
+	};
+
+	namespace ogg
+	{
+		
+	};
+
+	namespace mp3
+	{
+		static enum mad_flow input(void *data, struct mad_stream *stream);
+		static inline signed int scale(mad_fixed_t sample);
+		static enum mad_flow output(void *data, struct mad_header const *header, struct mad_pcm *pcm);
+		static enum mad_flow error(void *data, struct mad_stream *stream, struct mad_frame *frame);
+		static int decode(Sound *load_sound);
+		static bool IsMP3File(unsigned char *magic);
 	};
 
 	class SoundManager
@@ -28,21 +63,24 @@ namespace fr
 			}
 			void init();
 			void clear();
-			void clear(std::string path, SoundType type);
-			void load(std::string path, SoundType type);
-			void play(std::string path, SoundType type);
+			void clear(std::string path);
+			bool load(std::string path);
+			void play(std::string path);
 			void play(std::string path, int time);
 			void stop();	//music type only
 			void SwitchPause();	//music type only
 			void SetVolume(Uint16 load_volume, SoundType type);
+			void LoadMP3(std::string path);
+			void LoadOGG(std::string path);
+			static void AudioCallback(void *userdata, unsigned char *stream, int len);
 
 			bool IsPlayingMusic();
 		private:
 			SoundManager() {}
 			~SoundManager() {}
-			std::map<std::string, Mix_Music*> m_music;
-			std::map<std::string, Mix_Chunk*> m_sfx;
-			Uint16 SoundVolume;
+			static std::map<std::string, Sound*> m_sound;
+			static std::string current_sound_data;
+			static unsigned short SoundVolume;
 			static SoundManager *m_instance;
 	};	//class SoundManager
 };	//namespace fr
